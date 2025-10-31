0839e8afce8e4f085a7b19186e32acf08d0697e5
Anusha Yuvika <yuvikaanusha19@gmail.com>
2025-10-31 17:13:12 +0530

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (task.trim() === "") return;
+    setTodos([...todos, task]);
+    setTask("");
+  };
+
+  const deleteTodo = (index) => {
+    const updated = todos.filter((item, i) => {
+      return i !== index;   
+    });
+    setTodos(updated);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>To-Do-List</h1>
+      <input type="text" value={task} onChange={(e) => setTask(e.target.value)}/>
+      <button onClick={addTodo}>Add Todo</button>
+
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>
+            {item}  
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
